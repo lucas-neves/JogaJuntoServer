cfd2807f6477034516d482716e154a02f34010b4
lucasn580 <lucas_neves580@hotmail.com>
2016-11-09 14:38:04 -0200

    first

---
+++ webApi/Startup.cs.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Owin;
+using System.Web.Http;
+
+
+namespace webApi
+{
+	public static class Startup
+    {
+        public static void Configuration(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+			//Remove formato XML.
+			config.Formatters.Remove(config.Formatters.XmlFormatter);
+			//Indenta Json
+			config.Formatters.JsonFormatter.Indent = true;
+        }
+    }
+}
